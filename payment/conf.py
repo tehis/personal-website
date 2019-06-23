810951d5f6bf6de2e1f3b70a64fa5a4a68c6bbc7
Ali <ali.khoshtinattt@gmail.com>
2019-06-23 06:50:20 +0430

    Add original pdfs and change payment html

--- payment/conf.py
+++ payment/conf.py
@@ -3,6 +3,6 @@ from personal import settings
 
 
 API_KEY = '913b828a-be93-45c4-a83a-450c3049328a'
-AMOUNT = 100
-FREE_PDF_PATH = os.path.join(settings.BASE_DIR, 'payment', 'templates', '4-it.pdf')
-NOT_FREE_PDF_PATH = os.path.join(settings.BASE_DIR, 'payment', 'templates', 'itil.pdf')
+AMOUNT = 1000
+FREE_PDF_PATH = os.path.join(settings.BASE_DIR, 'payment', 'templates', 'about course.pdf')
+NOT_FREE_PDF_PATH = os.path.join(settings.BASE_DIR, 'payment', 'templates', 'summary.pdf')
